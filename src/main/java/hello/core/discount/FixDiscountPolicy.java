e1544f15476bd36abd626f3c2a6d017aad31e412
ted7088 <ted3131@naver.com>
2023-07-19 11:21:23 +0900

    commit

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -9,7 +9,7 @@ public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000; //1000원 할인
     @Override
     public int discount(Member member, int price) {
-        if(member.getGrade()== Grade.VIP){
+        if(member.getGrade() == Grade.VIP){
             return discountFixAmount;
         }else{
             return 0;
