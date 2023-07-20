8ee98fccbbd3e5d13342ddacc5b33293de6398d8
ted7088 <ted3131@naver.com>
2023-07-20 18:31:12 +0900

    commit

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -4,7 +4,6 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 
 public class RateDiscountPolicy implements DiscountPolicy {
-
     private int discountPercent = 10;
 
     @Override
