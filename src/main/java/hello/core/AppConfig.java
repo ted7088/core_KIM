de08d93c5614b810809700de45c4087d1c1cf9eb
ted7088 <ted3131@naver.com>
2023-07-21 11:09:25 +0900

    commit

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,19 +12,20 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {  //공연 기획자의 역할 외부에서 주입
 
     public MemberService memberService() {
-        return new MemberServiceImpl(memberRepopsitory());
+        return new MemberServiceImpl(membeerRepository());
     }
 
-    private static MemoryMemberRepository memberRepopsitory() {
+    private static MemoryMemberRepository membeerRepository() {
         return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(memberRepopsitory(), discountPolicy());
+        return new OrderServiceImpl(membeerRepository(), discountPolicy());
     }
 
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
